8555dc6ffd2e65ac0a3d1ba1c20a2ebead0f5929
Aidan Fahey <afahey2003@yahoo.com>
2025-08-10 22:39:18 -0400

    Add files via upload

---
+++ SnowmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowmanApp.swift
+//  Snowman
+//
+//  Created by Connor Dailey and Aidan Fahey on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct SnowmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
